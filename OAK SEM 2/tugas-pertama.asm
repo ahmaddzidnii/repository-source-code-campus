6b6ab2648a7b649b8eef6cf35ee3b7d40089d923
Ahmad Zidni Hidayat <138422717+ahmaddzidnii@users.noreply.github.com>
2024-04-26 21:32:03 +0700

    Update tugas-pertama.asm

--- OAK SEM 2/tugas-pertama.asm
+++ OAK SEM 2/tugas-pertama.asm
@@ -8,8 +8,11 @@ org 0100H
 
 MOV AX, 10H ;Memuat nilai 10H ke dalam register AX (A).
 MOV BX, 20H ;Memuat nilai 20H ke dalam register BX (B).
+
+XCHG AX, BX ;Menukar nilai antara register AX (A) dan BX (B).
                
 MOV CX, AX  ;Menyalin nilai dari register AX (A) ke register CX (C).
 ADD CX,BX   ;Menambahkan nilai register BX (B) ke dalam register CX (C).
 
-RET
+
+HLT         ;Berhenti (halt) untuk menghentikan eksekusi program.
