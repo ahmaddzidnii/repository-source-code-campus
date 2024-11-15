878e54587d93fb62f4e641ee35d7c3ea5cc6a68b
Ahmad Zidni Hidayat <138422717+ahmaddzidnii@users.noreply.github.com>
2024-11-15 14:31:40 +0700

    Update contoh-simulasi.js

--- Semester 3/Analisis Algoritma dan Struktur Data/Binary Search/contoh-simulasi.js
+++ Semester 3/Analisis Algoritma dan Struktur Data/Binary Search/contoh-simulasi.js
@@ -1,12 +1,14 @@
 function binarySearch(arr, target) {
   let left = 0;
   let right = arr.length - 1;
+  let iteration = 0;
 
   while (left <= right) {
+    iteration++;
     const mid = Math.floor((left + right) / 2);
 
     if (arr[mid] === target) {
-      return mid;
+      return { index: mid, iteration };
     }
 
     if (arr[mid] < target) {
@@ -15,22 +17,25 @@ function binarySearch(arr, target) {
       right = mid - 1;
     }
   }
-  return -1;
+
+  return { index: -1, iteration };
 }
 
 function linearSearch(arr, target) {
+  let iteration = 0;
   for (let i = 0; i < arr.length; i++) {
+    iteration++;
     if (arr[i] === target) {
-      return i;
+      return { index: i, iteration };
     }
   }
-  return -1;
+  return { index: -1, iteration };
 }
 
 const arr = [];
-const target = 10000;
+const target = 500;
 
-for (let i = 1; i <= 10000; i++) {
+for (let i = 1; i <= 1000; i++) {
   arr.push(i);
 }
 
@@ -39,12 +44,13 @@ const startTime = performance.now();
 const indexBinary = binarySearch(arr, target);
 const endTime = performance.now();
 
-if (indexBinary !== -1) {
-  console.log(`Elemen yang dicari berada pada index ke-${indexBinary}`);
+console.log(`Binary search membutuhkan ${indexBinary.iteration} iterasi`);
+
+if (indexBinary.index !== -1) {
+  console.log(`Elemen yang dicari berada pada index ke-${indexBinary.index}`);
 } else {
   console.log("Elemen tidak ditemukan");
 }
-
 console.log(`Time Binary Search: ${endTime - startTime} ms`);
 
 // Linear Search
@@ -52,10 +58,10 @@ const startTimeLinear = performance.now();
 const indexLinear = linearSearch(arr, target);
 const endTimeLinear = performance.now();
 
-if (indexLinear !== -1) {
-  console.log(`Elemen yang dicari berada pada index ke-${indexLinear}`);
+console.log(`Linear search membutuhkan ${indexLinear.iteration} iterasi`);
+if (indexLinear.index !== -1) {
+  console.log(`Elemen yang dicari berada pada index ke-${indexLinear.index}`);
 } else {
   console.log("Elemen tidak ditemukan");
 }
-
 console.log(`Time Linear Search: ${endTimeLinear - startTimeLinear} ms`);
