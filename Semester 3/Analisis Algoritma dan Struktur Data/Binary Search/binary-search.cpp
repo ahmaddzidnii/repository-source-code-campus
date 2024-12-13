aa673373e3ac63a787a6c7ea5de5adb90def2b51
Ahmad Zidni Hidayat <138422717+ahmaddzidnii@users.noreply.github.com>
2024-12-13 16:34:20 +0700

    Update binary-search.cpp

--- Semester 3/Analisis Algoritma dan Struktur Data/Binary Search/binary-search.cpp
+++ Semester 3/Analisis Algoritma dan Struktur Data/Binary Search/binary-search.cpp
@@ -1,70 +1,8 @@
-// #include <iostream>
-// #include <cmath>
-
-// using namespace std;
-
-// // Versi Sesuai Flowchart
-// int binarySearch(int arr[], int n, int find)
-// {
-//     int left = 0;
-//     int right = n - 1;
-//     while (left <= right)
-//     {
-//         int mid = floor((left + right) / 2);
-
-//         if (arr[mid] != find)
-//         {
-//             if (find < arr[mid])
-//             {
-//                 right = mid - 1;
-//             }
-//             else
-//             {
-//                 left = mid + 1;
-//             }
-//         }
-//         else
-//         {
-//             return mid;
-//         }
-//     }
-//     return -1;
-// }
-
-// int main()
-// {
-//     int n = 1000;
-//     int find = 1000;
-
-//     int *arr = (int *)malloc(n * sizeof(int));
-
-//     for (int i = 0; i < n; i++)
-//     {
-//         arr[i] = i + 1;
-//     }
-
-//     int result = binarySearch(arr, n, find);
-
-//     if (result == -1)
-//     {
-//         cout << "Element not found" << endl;
-//     }
-//     else
-//     {
-//         cout << "Element found at index: " << result << endl;
-//     }
-
-//     free(arr);
-
-//     return 0;
-// }
-
 #include <iostream>
 #include <cmath>
 
 using namespace std;
 
-// Versi Lebih Sederhana
 int binarySearch(int arr[], int n, int find)
 {
     int left = 0;
@@ -73,17 +11,20 @@ int binarySearch(int arr[], int n, int find)
     {
         int mid = floor((left + right) / 2);
 
-        if (arr[mid] == find)
+        if (arr[mid] != find)
         {
-            return mid;
-        }
-        else if (arr[mid] < find)
-        {
-            left = mid + 1;
+            if (find < arr[mid])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
         }
         else
         {
-            right = mid - 1;
+            return mid;
         }
     }
     return -1;
@@ -91,8 +32,8 @@ int binarySearch(int arr[], int n, int find)
 
 int main()
 {
-    int n = 10000000;
-    int find = 1;
+    int n = 1000;
+    int find = 1000;
 
     int *arr = (int *)malloc(n * sizeof(int));
 
@@ -105,11 +46,11 @@ int main()
 
     if (result == -1)
     {
-        cout << "Element not found" << endl;
+        cout << "Angka yang dicari tidak ditemukan" << endl;
     }
     else
     {
-        cout << "Element found at index: " << result << endl;
+        cout << "Angka ditemukan dan berada pada index ke : " << result << endl;
     }
 
     free(arr);
