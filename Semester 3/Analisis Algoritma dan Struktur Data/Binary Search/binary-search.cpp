b00fbb4bdf1ab32817a62e9217c5974d5a25c5e6
Ahmad Zidni Hidayat <138422717+ahmaddzidnii@users.noreply.github.com>
2024-12-30 21:30:26 +0700

    Update binary-search.cpp

--- Semester 3/Analisis Algoritma dan Struktur Data/Binary Search/binary-search.cpp
+++ Semester 3/Analisis Algoritma dan Struktur Data/Binary Search/binary-search.cpp
@@ -1,3 +1,7 @@
+// Ahmad Zidni Hidayat
+// 23106050077
+// Analisis Algoritma dan Struktur Data B
+
 #include <iostream>
 #include <cmath>
 
@@ -7,33 +11,45 @@ int binarySearch(int arr[], int n, int find)
 {
     int left = 0;
     int right = n - 1;
+    int step = 1;
     while (left <= right)
     {
         int mid = floor((left + right) / 2);
+
+        cout << "Langkah " << step << ":\n";
+        cout << "    Left Index = " << left << ", Right index =  " << right << ", Mid index = " << mid << "\n";
+        cout << "    Nilai di Mid = " << arr[mid] << "\n";
 
         if (arr[mid] != find)
         {
             if (find < arr[mid])
             {
+                cout << "    " << find << " lebih kecil dari " << arr[mid] << ", pindah ke kiri.\n";
                 right = mid - 1;
             }
             else
             {
+                cout << "    " << find << " lebih besar dari " << arr[mid] << ", pindah ke kanan.\n";
                 left = mid + 1;
             }
         }
         else
         {
+            cout << "    Elemen ditemukan pada indeks " << mid << "!\n";
             return mid;
         }
+
+        step++;
     }
+
+    cout << "Elemen tidak ditemukan setelah " << step - 1 << " langkah.\n";
     return -1;
 }
 
 int main()
 {
-    int n = 1000;
-    int find = 1000;
+    int n = 8;
+    int find = 2;
 
     int *arr = (int *)malloc(n * sizeof(int));
 
@@ -42,15 +58,23 @@ int main()
         arr[i] = i + 1;
     }
 
+    cout << "Mencari elemen " << find << " dalam array...\n\n";
+    cout << "Isi array: ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "\n\n";
+
     int result = binarySearch(arr, n, find);
 
     if (result == -1)
     {
-        cout << "Angka yang dicari tidak ditemukan" << endl;
+        cout << "\nElement tidak ditemukan" << endl;
     }
     else
     {
-        cout << "Angka ditemukan dan berada pada index ke : " << result << endl;
+        cout << "\nElement ditemukan pada indeks ke-: " << result << endl;
     }
 
     free(arr);
