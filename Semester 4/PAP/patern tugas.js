612679fc632318d1840330372d6aaf6c3e89f77c
Ahmad Zidni Hidayat <138422717+ahmaddzidnii@users.noreply.github.com>
2025-03-11 21:32:40 +0700

    Update patern tugas.js

--- Semester 4/PAP/patern tugas.js
+++ Semester 4/PAP/patern tugas.js
@@ -1,10 +1,13 @@
-const n = 2;
+const n = 1;
 
-for (let i = 0; i < n + 1; i++) {
+for (let i = 0; i <= n; i++) {
   console.log(`put a red square at (${i},${i})`);
   console.log(`put a red square at (${i},${i + 1})`);
 }
 
 if (n % 2 === 0) {
   console.log(`put a green square at (${n + 1},${n + 1})`);
+} else {
+ console.log(`put a blue square at (${n + 2},${n})`);
+ console.log(`put a blue square at (${n + 2},${n + 2})`);
 }
